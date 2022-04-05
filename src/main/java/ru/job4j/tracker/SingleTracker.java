2fdfad69aa2d611ed50d664eccc48382a691e3cc
miber66 <markkhokhlyavin@gmail.com>
2022-04-05 10:20:17 +0500

    1. Singleton [#94606 #284188]

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -12,28 +12,4 @@ public final class SingleTracker {
         }
         return instance;
     }
-
-    public Item add(Item item) {
-        return SingleTracker.getInstance().add(item);
-    }
-
-    public Item[] findAll() {
-        return SingleTracker.instance.findAll();
-    }
-
-    public Item[] findByName(String key) {
-        return SingleTracker.instance.findByName(key);
-    }
-
-    public Item findById(int id) {
-        return SingleTracker.instance.findById(id);
-    }
-
-    public boolean replace(int id, Item item) {
-        return SingleTracker.instance.replace(id, item);
-    }
-
-    public boolean delete(int id) {
-        return SingleTracker.instance.delete(id);
-    }
 }
