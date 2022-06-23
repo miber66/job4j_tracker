666f9a0fd20d9f3cbf94797f60e2c0023c374d7f
miber66 <markkhokhlyavin@gmail.com>
2022-06-23 07:01:56 +0500

    2. Аттестация [#49777]

---
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -0,0 +1,50 @@
+package ru.job4j.stream;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Analyze {
+    public static double averageScore(Stream<Pupil> stream) {
+        return stream.flatMap(p -> p.getSubjects()
+                .stream())
+                .mapToInt(Subject::getScore)
+                .average().orElse(0D);
+    }
+
+    public static List<Tuple> averageScoreBySubject(Stream<Pupil> stream) {
+        return stream.map(p -> new Tuple(p.getName(), ((p.getSubjects().stream())
+                .mapToInt(Subject::getScore)
+                .average()
+                .orElse(0D))))
+                .collect(Collectors.toList());
+    }
+
+    public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
+        return stream.flatMap(pupil -> pupil.getSubjects()
+                .stream()).collect(Collectors.groupingBy(Subject::getName, LinkedHashMap::new,
+                        Collectors.averagingDouble(Subject::getScore)))
+                        .entrySet().stream()
+                        .map(entry -> new Tuple(entry.getKey(), entry.getValue()))
+                        .collect(Collectors.toList());
+    }
+
+    public static Tuple bestStudent(Stream<Pupil> stream) {
+        return stream.map(pupil -> new Tuple(pupil.getName(), ((pupil.getSubjects().stream())
+                        .mapToDouble(Subject::getScore)
+                        .sum())))
+                        .max(Tuple::compareTo)
+                        .orElse(null);
+    }
+
+    public static Tuple bestSubject(Stream<Pupil> stream) {
+        return stream.flatMap(pupil -> pupil.getSubjects().stream())
+                .collect(Collectors.groupingBy(Subject::getName,
+                        Collectors.summingDouble(Subject::getScore)))
+                .entrySet().stream()
+                .map(entry -> new Tuple(entry.getKey(), entry.getValue()))
+                .max(Tuple::compareTo)
+                .orElse(null);
+    }
+}
---
+++ src/main/java/ru/job4j/stream/Tuple.java
@@ -0,0 +1,36 @@
+package ru.job4j.stream;
+
+import java.util.Objects;
+
+public class Tuple implements Comparable<Tuple> {
+    private String name;
+    private double score;
+
+    public Tuple(String name, double score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Tuple tuple = (Tuple) o;
+        return Double.compare(tuple.score, score) == 0
+                && Objects.equals(name, tuple.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, score);
+    }
+
+    @Override
+    public int compareTo(Tuple o) {
+        return Double.compare(this.score, o.score);
+    }
+}
